5cfd117d5ba2690eb1188d9d2d06948b72b5ddd0
Brent Christensen <brent@eventespresso.com>
2014-10-24 13:39:09 -0700

    add some HTML structure plus css ids and classes; loop through array of events and add buttons for each event; use filtered tempalte args instead of hard coded strings in content [touch:6319]

--- templates/espresso-social-buttons-template.template.php
+++ templates/espresso-social-buttons-template.template.php
@@ -1,8 +1,18 @@
-<h3 class="ee-registration-social_buttons-h3"><?php echo __('Support us on Social Media -- Spread the Word', 'event_espresso'); ?></h3>
+<div id="espresso-social-buttons-dv">
+	<h3 class="espresso-social-buttons-h3"><?php echo $heading; ?></h3>
 
-		<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $event_permalink; ?>" data-text="<?php echo sprintf(__('I just registered for %1s at %2s', 'event_espresso'), $event_name, $organization_name); ?>"  data-via="<?php echo $co_twitter; ?>" data-size="large"><?php echo __('Tweet', 'event_espresso'); ?></a>
-
-		<div id="fb-root"></div>
-
-
-		<div class="fb-like" data-href="<?php echo $event_permalink; ?>" data-send="true" data-width="450" data-show-faces="true"></div>
+	<?php foreach ( $events as $event_class => $event ) : ?>
+	<div id="espresso-social-buttons-dv-<?php echo $event_class; ?>" class="espresso-social-buttons-dv">
+		<?php if ( count( $events ) > 1 ) : ?>
+			<h5 class="espresso-social-buttons-event-name-h5"><?php echo $event['event_name']; ?></h5>
+		<?php endif; ?>
+		<div id="espresso-social-buttons-dv-twitter" class="espresso-social-button-dv">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $event['event_permalink']; ?>" data-text="<?php echo $tweet_message; ?>"  data-via="<?php echo $event['co_twitter']; ?>" data-size="large"><?php echo __('Tweet', 'event_espresso'); ?></a>
+		</div>
+		<div id="espresso-social-buttons-dv-facebook" class="espresso-social-button-dv">
+			<div id="fb-root"></div>
+			<div class="fb-like" data-href="<?php echo $event['event_permalink']; ?>" data-send="true" data-width="450" data-show-faces="true"></div>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</div>
